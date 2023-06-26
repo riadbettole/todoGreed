f21c51caf9443ab556eaf75a1274e5a7d8acc783
riadbettole <riadbettole@gmail.com>
2023-06-26 05:12:09 +0100

    w

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
 import jwt_decode from "jwt-decode";
-import {useEffect} from "react"; 
+import {useEffect, useState} from "react"; 
 
 function App() {
   // const [authToken, setAuthToken] = useState('');
 
+  const [email, setEmail] = useState('');
   useEffect(() => {
     const currentURL = window.location.href;
     const params = currentURL.split('#')[1];
     const decodedToken = jwt_decode(params);
-
+    console.log(params);
     console.log(decodedToken);
-
+    setEmail(decodedToken.username)
   }, []);
 
   return (
       <div className="App">
-        Bonjour
+        Bonjour {email}
       </div>
   );
 }
